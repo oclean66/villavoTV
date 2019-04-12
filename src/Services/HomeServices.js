77813cf1350ad67eea4a3c6673c5c4e9ce761303
Jaylerth <oclean66@gmail.com>
2019-04-12 15:35:03 -0500

    20

--- src/Services/HomeServices.js
+++ src/Services/HomeServices.js
@@ -3,7 +3,7 @@ import { API_URI, CLIENT_ID } from '../config';
 export default {
     async getShowsList() {
 
-        let response = await fetch(API_URI + '/shows/trending?limit=50', {
+        let response = await fetch(API_URI + '/shows/trending?limit=20', {
             method: "GET",
             headers: {
                 "trakt-api-key": CLIENT_ID,
